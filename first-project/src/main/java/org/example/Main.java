56e043188cf7382fad2eaf59c3f6a01629fd38d2
sevagog <seva75699@mail.ru>
2022-11-29 12:11:04 +0300

    Did something

--- first-project/src/main/java/org/example/Main.java
+++ first-project/src/main/java/org/example/Main.java
@@ -1,33 +1,21 @@
 package org.example;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int a = -1;
-        int b = 2;
-        double d = 2.2;
-        float f = 2.2f;
-        String s = "just-string";
-        boolean l = true;
-//        System.out.println(a + b);
+        Scanner scanner = new Scanner(System.in);
+        int maxi = Integer.MIN_VALUE, a[] = new int[4];
+        a[0] = scanner.nextInt();
+        a[1] = scanner.nextInt();
+        a[2] = scanner.nextInt();
+        a[3] = scanner.nextInt();
 
-//        if (a > 0) {
-//            System.out.println(a);
-//        }else{
-//            System.out.println("a<=0");
-//        }
-//        int i = 0;
-//        while(i < 10){
-//            System.out.println(i);
-//            i++;
-//        }
+        for(int i = 0; i < 4; i++){
+            if(maxi < a[i]){
+                maxi = a[i];
+            }
+        }
 
-        Human human = new Human(30, 180);
-        Human human1 = new Human(29, 179);
-        Human baby1 = new Human(55);
-
-
-        System.out.println(baby1.olderThan18());
-        System.out.println(human.getAge());
-
+        System.out.println(maxi);
     }
 }
